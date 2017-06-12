678c96cba4520ad03a45a53208418079c7b04a19
hfrankst <harperfrankstone@gmail.com>
2017-06-12 15:57:16 -0500

    worked on scraping and narrowing down the data being brought in

--- NSS-frontend-capstone-master/scrape/kroger.py
+++ NSS-frontend-capstone-master/scrape/kroger.py
@@ -5,34 +5,40 @@ from bs4 import BeautifulSoup
 headers = {
     'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36'
 }
-
 # target URL
-url = "http://www.kroger.com/weeklyad?StoreCode=00542&DivisionId=029"
+url = "http://grocerysmarts.com/tenn/lists/indexmemphis.php?a37ll2"
+# will need to increase the page number with a for loop
+
+# https://www.kroger.com/weeklyad?StoreCode=00511&DivisionId=026
+
 
 # get the request object containing all info to get ready for the soup
-try:
-    request = requests.get(url, headers=headers, timeout=5)
-except requests.exceptions.Timeout:
-    print("Timeout occurred")
+response = requests.get(url, timeout=10)
+# print(request)
+# catching a bad download
+# try:
+#     requests.raise_for_status()
+# except Exception as exc:
+#     print('There was a problem: %s' % (exc))
 
-# .text grabs the html from the object
-data = request.text
+soup = BeautifulSoup(response.content, "html.parser")
 
-# passing the request object into BS and parses it as html
-soup = BeautifulSoup(data, "html.parser")
-# print(soup)
+table_cells = soup.find_all(attrs = {'width': ['228', '55']})
+# print(table_cells)
 
-# soup_list = soup.find(class='item')
-# print(soup_list)
+for cell in table_cells:
+    for i in cell:
+        table_data = str(i).replace('<td valign="top" width="228"><b>', '').replace('<td valign="top" width="228">', '').replace('</td>', '').replace('<br/>', '').replace('<b>', '').replace('</b>', '')
+        print(table_data)
 
+# grabs the html from the object and writes to a txt file that will be passed into beautiful soup
+# with open('kroger.txt', 'wb') as data:
 
-# brings back the BS types that all the elements of the page fall into
-types = [type(item) for item in  list(soup.children)]
-# print('soup types on this webpage: ', types)
+#     for chunk in soup.select('.item'):
+#         print(chunk)
 
-# brings back the Tag element, I'm not sure what the attributes of this are though
-tag_list = types[8]
-# print(tag_list)
+# print(data)
+
 
 
 
