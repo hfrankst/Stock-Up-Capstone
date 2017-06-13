55e42e88ff82bbd4a112530fbbad1c5d73925e15
hfrankst <harperfrankstone@gmail.com>
2017-06-13 16:01:57 -0500

    the shape of the data is closer to what I need, really need to finish this part tomorrow

--- NSS-frontend-capstone-master/scrape/kroger.py
+++ NSS-frontend-capstone-master/scrape/kroger.py
@@ -13,25 +13,23 @@ response = requests.get(url, timeout=10)
 
 soup = BeautifulSoup(response.content, "html.parser")
 
+# target the specific cells that hold the product name, and product price.  widths below correspond respectively
 table_cells = soup.find_all(attrs = {'width': ['228', '55']})
 
+# looping over all the cells in the table to wittle down the data to just kroger deals
 for cell in table_cells:
-    kroger_data = cell.get_text()
+    # this series of replace methods takes out all the lines that are not products and those products' prices
+    kroger_data = cell.get_text().rstrip("Got a list question? Want to check if next week's list is ready?  Our listmakers answer questions and post our \"preview\" lists at PYP.").replace('Kroger', '').replace('Todays date:  How to use this list ', '').replace('Current list-This list is valid during the dates listed b', '').replace('This weeks FREE Friday Download is Pure Leaf Tea House Collection Tea 14 oz', '').replace('Buy 5 of the following products and save $5.00', '')
+    # this if statement stops the loop from including the CVS section
     if 'CVS' in kroger_data:
-            break
-    else: 
-        print(kroger_data)
+        break
+    else:
+        # trying to iterate over the narrowed data to grab the names and prices
+        for each in kroger_data:
+            # currently this is printing out each character of the kroger_data, which is one giant string.  Maybe I need to make it a list again?
+            print(each)
 
 
 
 
 
-
-
-
-
-
-
-        # I need a way to look for this bit of code '<p align="left">' and stop once that is seen
-        # I need to figure out a way to stop the loop once it reaches the 'cvs' section.  Look at a while loop to accomplish this. Focus on only getting the kroger info, and then later get info from cvs, wallgreens and any others that are available. Also, learn how to incorporate the scraped data into the django framework. 
-        # need to figure out how to stop the loop 
