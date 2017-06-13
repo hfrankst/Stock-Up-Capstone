34cb33a48bb7ed72a4addf558dca9aa4b5a2b62f
hfrankst <harperfrankstone@gmail.com>
2017-06-13 11:53:08 -0500

    narrowing down the output of the scrape

--- NSS-frontend-capstone-master/scrape/kroger.py
+++ NSS-frontend-capstone-master/scrape/kroger.py
@@ -7,38 +7,31 @@ headers = {
 }
 # target URL
 url = "http://grocerysmarts.com/tenn/lists/indexmemphis.php?a37ll2"
-# will need to increase the page number with a for loop
-
-# https://www.kroger.com/weeklyad?StoreCode=00511&DivisionId=026
-
 
 # get the request object containing all info to get ready for the soup
 response = requests.get(url, timeout=10)
-# print(request)
-# catching a bad download
-# try:
-#     requests.raise_for_status()
-# except Exception as exc:
-#     print('There was a problem: %s' % (exc))
 
 soup = BeautifulSoup(response.content, "html.parser")
 
 table_cells = soup.find_all(attrs = {'width': ['228', '55']})
-# print(table_cells)
 
 for cell in table_cells:
-    for i in cell:
-        table_data = str(i).replace('<td valign="top" width="228"><b>', '').replace('<td valign="top" width="228">', '').replace('</td>', '').replace('<br/>', '').replace('<b>', '').replace('</b>', '')
-        print(table_data)
-
-# grabs the html from the object and writes to a txt file that will be passed into beautiful soup
-# with open('kroger.txt', 'wb') as data:
-
-#     for chunk in soup.select('.item'):
-#         print(chunk)
-
-# print(data)
+    kroger_data = cell.get_text()
+    if 'CVS' in kroger_data:
+            break
+    else: 
+        print(kroger_data)
 
 
 
 
+
+
+
+
+
+
+
+        # I need a way to look for this bit of code '<p align="left">' and stop once that is seen
+        # I need to figure out a way to stop the loop once it reaches the 'cvs' section.  Look at a while loop to accomplish this. Focus on only getting the kroger info, and then later get info from cvs, wallgreens and any others that are available. Also, learn how to incorporate the scraped data into the django framework. 
+        # need to figure out how to stop the loop 
